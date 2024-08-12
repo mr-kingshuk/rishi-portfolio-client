809feb7f290dbca3f23576bc0d7a6e9d54216854
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2024-08-12 22:39:40 +0530

    consoel.log production

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 
 const About = () => {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  console.log(API_BASE_URL);
   const [general, setGeneral] = useState({
     brief: "",
     resume: ""
